b71f44bf2ebb94a6af3c8152378f50429de4fcc5
Michael Ng <78869630+michwng@users.noreply.github.com>
2021-11-03 11:22:23 -0400

    Add files via upload

---
+++ Introduction to Computer Science/Fall 2020/Labs/Lab05/Driver.java
@@ -0,0 +1,15 @@
+/**
+ * The Driver class creates a Pet object and uses
+ * the pet object's printPet() method.
+ * 
+ * @author Michael Ng - ngmw01@etsu.edu
+ * @version 9/24/2020
+ */
+public class Driver 
+{
+    public static void main(String[] args)
+    {
+        Pet fluffy = new Pet();
+        fluffy.printPet();
+    }
+}
---
+++ Introduction to Computer Science/Fall 2020/Labs/Lab05/Pet.java
@@ -0,0 +1,15 @@
+/**
+ * The Pet class has one method called printPet() that prints 
+ * "I have a pet ostrich named Chandler".
+ * 
+ * @author Michael Ng - ngmw01@etsu.edu
+ * @version 9/24/2020
+ */
+class Pet
+{
+    //prints "I have a pet ostrich named Chandler"
+    public void printPet()
+    {
+        System.out.println("I have a pet ostrich named Chandler");
+    }
+}
